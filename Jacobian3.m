ede8eac5c6537e8077ecae34cc298f36559b3fb4
Stanisław <62651497+staszkiet@users.noreply.github.com>
2024-12-13 12:29:15 +0100

    Update Jacobian3.m

--- Jacobian3.m
+++ Jacobian3.m
@@ -1,10 +1,3 @@
 function[Z] = Jacobian3(x, y)
-%m = zeros(2, 2);
-%m(1, 1) = cos(pi*(y + 1))/2;
-%m(1, 2) = -(1/2)*(x + 1)*sin(pi*(y + 1))*pi;
-%m(2, 1) = sin(pi*(y + 1))/2;
-%m(2, 2) = (1/2)*(x + 1)*cos(pi*(y + 1))*pi;
-%Z = det(m);
-%Z = cos(pi.*(y + 1))/2.*(1/2).*(x + 1).*cos(pi.*(y + 1)).*pi + (1/2).*(x + 1).*sin(pi.*(y + 1))*pi.*sin(pi.*(y + 1))/2;
 Z = pi*(x + 1)/4;
 end

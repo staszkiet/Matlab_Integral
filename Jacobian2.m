5b9c32f2c8cda1b9a8219695f7a618afdb38ce20
Stanisław <62651497+staszkiet@users.noreply.github.com>
2024-12-13 12:28:04 +0100

    Update Jacobian2.m

--- Jacobian2.m
+++ Jacobian2.m
@@ -1,6 +1,4 @@
 function[Z] = Jacobian2(x, y)
-% Projekt 1, Zadanie 36
-% Stanisław Zaprzalski, 327415
 % 
 % Funkcja oblicza jakobian przekształceń fi na pierwszą współrzędną i
 % psi na drugą współrzędną. Zwraca wartość jakobianu dla (x, y)

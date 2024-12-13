1a55d4229dc3a83d583a142da2675dfa9cee4d23
Stanisław <62651497+staszkiet@users.noreply.github.com>
2024-12-13 12:30:49 +0100

    Update fi.m

--- fi.m
+++ fi.m
@@ -1,6 +1,4 @@
 function[z] = fi(x, y)
-% Projekt 1, Zadanie 36
-% Stanisław Zaprzalski, 327415
 % 
 % Funkcja, która przekształca pierwszą współrzędną w transofrmacji obszaru 
 % [-1, 1] x [1, 1] na koło o środku w (0, 0) i promieniu 1

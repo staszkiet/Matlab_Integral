5dd8fd579cde734db341a794078e3345c554df65
Stanisław <62651497+staszkiet@users.noreply.github.com>
2024-12-13 12:30:58 +0100

    Update func.m

--- func.m
+++ func.m
@@ -1,6 +1,4 @@
 function[Z] = func(f, x, y)
-% Projekt 1, Zadanie 36
-% Stanisław Zaprzalski, 327415
 % 
 % Funkcja zwraca wartość f(fi(x, y), psi(x, y)).*abs(Jacobian2(x, y))
 % dla odpowiednich wartości x i y oraz odpowiedniej funkcji f.

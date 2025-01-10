906bea3b7fe95e847885c0fefe01f67eea0c7072
Stanisław <62651497+staszkiet@users.noreply.github.com>
2025-01-10 10:46:47 +0100

    Update p.m

--- p.m
+++ p.m
@@ -1,6 +1,4 @@
 function[z] = p(x, y)
-% Projekt 1, Zadanie 36
-% Stanisław Zaprzalski, 327415
 % 
 % Funkcja, która przekształca drugą współrzędną w transofrmacji obszaru 
 % [-1, 1] x [1, 1] na koło o środku w (0, 0) i promieniu 1
